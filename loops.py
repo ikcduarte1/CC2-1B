db5d8e55f86a8d92105a0df77609123f739145a5
ikcduarte1 <145998151+ikcduarte1@users.noreply.github.com>
2023-10-09 08:27:25 +0800

    Update loops.py

--- loops.py
+++ loops.py
@@ -1,17 +1,20 @@
+#NUMBER 1
 for i in range(5):
     for j in range(1, i + 2):
         print(j, end = " ")
     print()
 
+#NUMBER 2
+number = int(input("input: "))
+sum = 0
+for i in range(number + 1):
+    sum += i
+print(sum)
 
+#NUMBER 3
 for i in range(5, 0, -1):
     for j in range(1, i + 1):
         print(j, end = " ")
     print()
 
 
-number = int(input("input: "))
-sum = 0
-for i in range(number + 1):
-    sum += i
-print(sum)
