7b37ef307ce9c372206bb939ab75c19619115e0e
ikcduarte1 <145998151+ikcduarte1@users.noreply.github.com>
2023-11-08 11:41:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,75 @@ import random
 
 
 
+
+
+class Weapons:
+    def __init__(self, name, damage):
+        self.name = name
+        self.damage = damage
+    def attack(self):
+        print(f"Your character used {self.name} to attack and dealth {self.damage} damage!")
+    class CurrentWeapon:
+        def __init__(self, current_weapon):
+            self.current_weapon = current_weapon
+        def replace(self, new_weapon):
+            self.current_weapon = new_weapon
+            print(f"Replaced current weapon to {new_weapon.name}")
+
+
+Broom = Weapons("Broom", random.randint(5,10))
+WaterGun = Weapons("Water Gun", random.randint(10,20))
+Vaccum = Weapons("Vaccum", random.randint(30,50))
+BleachBane = Weapons("Bleach Bane", random.randint(50, 100))
+
 class Enemy:
+    def __init__(self, name, damage, health):
+        self.name = name
+        self.damage = damage
+        self.health = health
+    
+    def attack(self):
+        print(f"The {self.name} attacks you for {self.damage} damage")
+
+    def attacked(self):
+        print(f"The current health of {self.name} is {self.health}")
+        if self.health <= 0:
+            print(f"The {self.name} has died")
+    
+
+Pollutant = Enemy("Pollutant", 5, 20)
+Toxigore = Enemy("Toxigore", 10, 15)
+Contaminoid = Enemy("Contaminoid", 100, 50)
+Pollutiax = Enemy("Pollutiax", 20, 500)
+
+
+class Treasures:
     ...
 
 
+class Character:
+    
+    alive = True
+    def __init__(self, health):
+        self.health = health
+    
+    def attacked(self):
+        
+        print(f"Your current health is {self.health}")
+        if self.health <= 0:
+            print("You have DIED!")
+            alive = False
+
+
+myCharacter = Character(100)
+
 def main():
     prompt()
+
+    if Character.alive == False:
+        try_again = input("Play again?(Y/N)").upper()
+        if try_again == 'Y':
+            prompt
 
 
 
@@ -56,7 +119,7 @@ Your quest is to vanquish the vile forces of dirt and grime, and cleanse the wor
 
 
 
-def print_text_slowly(text, delay=0.05):
+def print_text_slowly(text, delay=0.03):
     for char in text:
         print(char, end='', flush=True)
         time.sleep(delay)
